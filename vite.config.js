d21be53fb9c27f8c157ab64d632e832eb503ed3c
Maurilio Siqueira <mauriliosiqueira2001@hotmail.com>
2024-12-08 21:30:25 -0300

    Arrumkando vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Caminho base (deixe como '/' se estiver implantando na raiz)
   server: {
-    host: '0.0.0.0', // Configura o servidor para escutar em todas as interfaces de rede
+    host: '0.0.0.0', // Escutar em todas as interfaces de rede
   },
-})
+  build: {
+    outDir: 'dist', // Diretório de saída para o build (padrão do Vite)
+  },
+});
